2e65a8057619bd4a8677e71cfd985b7f584fbba4
ShuaiZhang06 <zhang.s3@northeastern.edu>
2025-12-06 23:30:54 -0500

    Update LogisticsOrganization.java

--- OrganLink-Heros-Final/src/Business/Organization/LogisticsOrganization.java
+++ OrganLink-Heros-Final/src/Business/Organization/LogisticsOrganization.java
@@ -1,18 +1,26 @@
 
 package Business.Organization;
 
+import Business.Logistics.LogisticsTeamDirectory;
+import Business.Logistics.TransportVehicleDirectory;
 import Business.Role.LogisticsRole;
 import Business.Role.Role;
 import java.util.ArrayList;
 
 /**
  *
- * @author rrheg
+ * @author eric
  */
 public class LogisticsOrganization extends Organization {
     
+    // add TransportVehicleDirectory and LogisticsTeamDirectory
+    private TransportVehicleDirectory vehicleDirectory;
+    private LogisticsTeamDirectory teamDirectory;
+    
     public LogisticsOrganization() {
         super(Type.Logistics.getValue());
+        vehicleDirectory = new TransportVehicleDirectory();
+        teamDirectory = new LogisticsTeamDirectory();
     }
     
     @Override
@@ -22,4 +30,13 @@ public class LogisticsOrganization extends Organization {
         return roles;
     }
     
+    // Getter for vehicle directory
+    public TransportVehicleDirectory getVehicleDirectory() {
+        return vehicleDirectory;
+    }
+    
+    // Getter for team directory
+    public LogisticsTeamDirectory getTeamDirectory() {
+        return teamDirectory;
+    }
 }
